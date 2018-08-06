6b477b11e9d68e6cb338d0c1f36d6089a1ec7ef0
walkward <walker.ward1@gmail.com>
2018-08-06 11:43:00 -0600

    adding nyc for test coverage

--- internals/shell/test.sh
+++ internals/shell/test.sh
@@ -2,7 +2,7 @@
 
 npm run cleanup:reports
 
-# Run tests...
+# Run tests and generate test coverage
 set -x
 mkdir -p ${DIR}/reports
-ava-ts --tap | tap-xunit > ${DIR}/reports/ava-junit.xml
+nyc ava-ts --tap | tap-xunit > ${DIR}/reports/ava-junit.xml
