a501ea4c1f6bd156e29c40e7a2bbdd8bd228a789
walkward <walker.ward1@gmail.com>
2018-08-06 09:27:55 -0600

    removing unecessary tslint comments

--- src/tests/example.test.ts
+++ src/tests/example.test.ts
@@ -4,10 +4,8 @@
 
 import test from 'ava';
 
-// tslint:disable-next-line:typedef
 const fn = async () => Promise.resolve('foo');
 
-// tslint:disable-next-line:typedef
 test('Example Test', async (t) => {
   t.is(await fn(), 'foo');
 });
