8998abb796d2d4e8d69a3738694032be4b205828
walkward <walker.ward1@gmail.com>
2018-08-06 09:28:09 -0600

    adding test generator

--- internals/generators/file/index.js
+++ internals/generators/file/index.js
@@ -5,11 +5,21 @@
 const path = require('path');
 
 module.exports = {
-  description: 'new typescript file',
+  description: 'Generate typescript scaffolding',
   prompts: [{
+    type: 'list',
+    name: 'type',
+    message: 'Select the type of file',
+    default: 'function',
+    choices: () => [
+      'controller',
+      'model',
+      'test',
+    ],
+  }, {
     type: 'input',
     name: 'name',
-    message: 'Enter the file name',
+    message: 'Enter a file name',
     validate: filename => {
       if (/^([^\s.\x00-\x1F!"$'\(\)*,\/:;<>\?\[\\\]\{\|\}\x7F]+)$/.test(filename)) return true;
       return 'Invalid filename';
@@ -17,16 +27,27 @@ module.exports = {
   }, {
     type: 'input',
     name: 'summary',
-    message: 'Enter summary of file contents',
+    message: answers => {
+      switch (answers.type) {
+      case 'test':
+        return 'Describe what will be tested';
+      case 'model':
+        return 'Describe this object and how it will be used';
+      case 'controller':
+        return 'Describe what will be controlled';
+      default:
+        return 'Enter a short summary of the file contents';
+      }
+    },
   }, {
-    type: 'list',
-    name: 'type',
-    message: 'Select the type of export',
-    default: 'function',
-    choices: () => [
-      'controller',
-      'model',
-    ],
+    type: 'confirm',
+    name: 'generateTest',
+    message: 'Generate tests?',
+    default: false,
+    when: answers => {
+      if (answers.type !== 'test') return true;
+      return false;
+    },
   }, {
     type: 'confirm',
     name: 'restMethods',
@@ -37,23 +58,31 @@ module.exports = {
       return false;
     },
   }],
-  actions: data => {
-    let templateFile, filePath;
+  actions: answers => {
+    const actionTypes = {
+      test: {
+        type: 'add',
+        templateFile: path.join(__dirname, './test.js.hbs'),
+        path: path.join(__dirname, '../../../src/tests/{{ camelCase name }}.test.ts'),
+      },
+      model: {
+        type: 'add',
+        templateFile: path.join(__dirname, './class.js.hbs'),
+        path: path.join(__dirname, '../../../src/models/{{ properCase name }}.ts'),
+      },
+      controller: {
+        type: 'add',
+        templateFile: path.join(__dirname, './function.js.hbs'),
+        path: path.join(__dirname, '../../../src/controllers/{{ camelCase name }}.ts'),
+      },
+    };
 
-    switch (data.type) {
-    case 'model':
-      templateFile = path.join(__dirname, './class.js.hbs');
-      filePath = path.join(__dirname, '../../../src/models/{{ properCase name }}.ts');
-      break;
-    default:
-      templateFile = path.join(__dirname, './function.js.hbs');
-      filePath = path.join(__dirname, '../../../src/controllers/{{ camelCase name }}.ts');
-    }
+    // Assigning test
+    const actions = [actionTypes[answers.type]];
 
-    return [{
-      type: 'add',
-      path: filePath,
-      templateFile,
-    }];
+    // Check if user has requested tests to be generated
+    if (answers.generateTest) actions.push(actionTypes.test);
+
+    return actions;
   },
 };
